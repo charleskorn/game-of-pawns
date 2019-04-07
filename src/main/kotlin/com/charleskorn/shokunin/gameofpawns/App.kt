f07400e9d8b945c4ac60dae25c8778324b32611c
Charles Korn <ckorn@thoughtworks.com>
2019-04-07 17:57:04 +1000

    Add application entry point.

--- src/main/kotlin/com/charleskorn/shokunin/gameofpawns/App.kt
+++ src/main/kotlin/com/charleskorn/shokunin/gameofpawns/App.kt
@@ -1,5 +1,38 @@
 package com.charleskorn.shokunin.gameofpawns
 
+import com.charleskorn.shokunin.gameofpawns.printing.BoardPrinter
+import com.charleskorn.shokunin.gameofpawns.printing.FENPrinter
+import com.charleskorn.shokunin.gameofpawns.printing.PlainTextPrinter
+import java.io.PrintStream
+import kotlin.system.exitProcess
+
 fun main(args: Array<String>) {
-    println("Hello world!")
+    exitProcess(runApplication(args, System.out, System.err))
+}
+
+fun runApplication(args: Array<String>, out: PrintStream, err: PrintStream): Int {
+    if (args.size > 1) {
+        return showArgsErrorAndExit(err)
+    }
+
+    when (args.firstOrNull()) {
+        "--grid" -> printRandomBoard(PlainTextPrinter(), out)
+        "--fen", null -> printRandomBoard(FENPrinter(), out)
+        else -> return showArgsErrorAndExit(err)
+    }
+
+    return 0
+}
+
+private fun showArgsErrorAndExit(err: PrintStream): Int {
+    err.println("Error: this application takes at most one argument, either --grid or --fen.")
+
+    return -1
+}
+
+private fun printRandomBoard(printer: BoardPrinter, out: PrintStream) {
+    val generator = BoardGenerator()
+    val board = generator.generate()
+
+    out.println(printer.print(board))
 }
---
+++ src/test/kotlin/com/charleskorn/shokunin/gameofpawns/AppSpec.kt
@@ -0,0 +1,87 @@
+package com.charleskorn.shokunin.gameofpawns
+
+import ch.tutteli.atrium.api.cc.en_GB.atLeast
+import ch.tutteli.atrium.api.cc.en_GB.atMost
+import ch.tutteli.atrium.api.cc.en_GB.contains
+import ch.tutteli.atrium.api.cc.en_GB.containsRegex
+import ch.tutteli.atrium.api.cc.en_GB.exactly
+import ch.tutteli.atrium.api.cc.en_GB.notToBe
+import ch.tutteli.atrium.api.cc.en_GB.regex
+import ch.tutteli.atrium.api.cc.en_GB.toBe
+import ch.tutteli.atrium.verbs.assert
+import org.spekframework.spek2.Spek
+import org.spekframework.spek2.style.specification.describe
+import java.io.ByteArrayOutputStream
+import java.io.PrintStream
+
+object AppSpec : Spek({
+    describe("the application") {
+        mapOf(
+                "given no arguments" to arrayOf(),
+                "given the single argument '--fen'" to arrayOf("--fen")
+        ).forEach { description, args ->
+            describe(description) {
+                val output = ByteArrayOutputStream()
+                val error = ByteArrayOutputStream()
+
+                val exitCode = runApplication(args, PrintStream(output), PrintStream(error))
+
+                it("exits with a zero exit code") {
+                    assert(exitCode).toBe(0)
+                }
+
+                it("prints a FEN-formatted board to stdout") {
+                    assert(output.toString()).containsRegex("""^([0-9pnbrqkPNBRQK]+/){7}[0-9pnbrqkPNBRQK]+ w - - 0 1$""")
+                }
+
+                it("prints nothing to stderr") {
+                    assert(error.toString()).toBe("")
+                }
+            }
+        }
+
+        describe("given the single argument '--grid'") {
+            val args = arrayOf("--grid")
+            val output = ByteArrayOutputStream()
+            val error = ByteArrayOutputStream()
+
+            val exitCode = runApplication(args, PrintStream(output), PrintStream(error))
+
+            it("exits with a zero exit code") {
+                assert(exitCode).toBe(0)
+            }
+
+            it("prints a text-formatted board to stdout") {
+                assert(output.toString()).containsRegex("""^(([pnbrqkPNBRQK.]\s\s){7}[pnbrqkPNBRQK.]\n){8}$""")
+            }
+
+            it("prints nothing to stderr") {
+                assert(error.toString()).toBe("")
+            }
+        }
+
+        mapOf(
+                "given another argument" to arrayOf("--something"),
+                "given multiple arguments" to arrayOf("--fen", "--grid")
+        ).forEach { description, args ->
+            describe(description) {
+                val output = ByteArrayOutputStream()
+                val error = ByteArrayOutputStream()
+
+                val exitCode = runApplication(args, PrintStream(output), PrintStream(error))
+
+                it("exits with a zero exit code") {
+                    assert(exitCode).notToBe(0)
+                }
+
+                it("prints nothing to stdout") {
+                    assert(output.toString()).toBe("")
+                }
+
+                it("prints nothing to stderr") {
+                    assert(error.toString()).toBe("Error: this application takes at most one argument, either --grid or --fen.\n")
+                }
+            }
+        }
+    }
+})
